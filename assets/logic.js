d13b47732c4340085b59e8d6d5176b7d279a1abc
Andrew Weihe <andrewy03@gmail.com>
2019-11-22 23:34:23 -0600

    adding function to js

--- assets/logic.js
+++ assets/logic.js
@@ -4,4 +4,9 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 //adding time blocks
 var hour = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
-
+//function to loop each element and to set the classes, & attribute.
+hour.forEach(function(time){
+    var text = $("<textarea>").attr("dataStorage", time);
+    var btn = $("<button>").addClass("saveBtn fas fa-save");
+    var row = $("<div>").addClass("row");
+    var hours = $("<div>").addClass("hour");
